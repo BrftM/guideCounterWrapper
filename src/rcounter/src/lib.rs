f636b8c05c52ba824b58b90812998b95930e56c3
BrftM <moritz.broft@student.uibk.ac.at>
2025-08-26 20:28:15 +0200

    Revert-to-minimal-changes-of-return-type

--- src/rcounter/src/lib.rs
+++ src/rcounter/src/lib.rs
@@ -1,36 +1,32 @@
 use extendr_api::prelude::*;
-use std::process::{Command};
+use std::process::Command;
 
-/// Exposes the guidecounter_count functionality as a Rust function callable from R.
-/// 
-/// @param input A character vector of input file paths.
-/// @param library A string specifying the library name or path to use.
-/// @param offset_min_fraction A numeric value specifying the minimum offset threshold.
-/// @param output A string specifying the output file path.
-/// @return A character string indicating success or error message.
-/// @export
 #[extendr]
 fn guidecounter_count(
-    input: Vec<String>,             
-    library: String,                  
-    offset_min_fraction: f64,          
-    output: String                     
+    input: Vec<String>,
+    library: String,
+    offset_min_fraction: f64,
+    output: String,
 ) -> RobjResult<String> {
     let mut command = Command::new("guide-counter");
     command.arg("count");
 
     for input_path in input {
-                command.arg("--input").arg(input_path);
+        command.arg("--input").arg(input_path);
     }
 
-    command.arg("--offset-min-fraction").arg(offset_min_fraction.to_string())
-            .arg("--library").arg(library)
-            .arg("--output").arg(output);
+    command
+        .arg("--offset-min-fraction")
+        .arg(offset_min_fraction.to_string())
+        .arg("--library")
+        .arg(library)
+        .arg("--output")
+        .arg(output);
 
     match command.output() {
         Ok(out) => {
-            if output.status.success() {
-                "Demux operation completed successfully.".to_string()
+            if out.status.success() {
+                Ok("Demux operation completed successfully.".to_string())
             } else {
                 let code = out.status.code().unwrap_or(-1);
                 let err_msg = String::from_utf8_lossy(&out.stderr).to_string();
